8cf7bd1a26f3346e7315d46eef3f15d642d671bc
eldor-galiev <eldor.galiev888@gmail.com>
2022-05-19 22:08:20 +0300

    version 1.1

---
+++ src/main/java/com/example/flappybird/Controller.java
@@ -0,0 +1,132 @@
+package com.example.flappybird;
+
+import javafx.animation.AnimationTimer;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+    @FXML
+    public ImageView start_button;
+    AnimationTimer gameLoop;
+
+    @FXML
+    private AnchorPane plane;
+
+    @FXML
+    private ImageView bird;
+
+    @FXML
+    private Text score;
+
+    private double accelerationTime = 0;
+    private int gameTime = 0;
+    private int scoreCounter = 0;
+
+    private Bird birdComponent;
+    private ObstaclesHandler obstaclesHandler;
+
+    ArrayList<Rectangle> obstacles = new ArrayList<>();
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        int jumpHeight = 50;
+        birdComponent = new Bird(bird, jumpHeight);
+        double planeHeight = 600;
+        double planeWidth = 400;
+        obstaclesHandler = new ObstaclesHandler(plane, planeHeight, planeWidth);
+
+        gameLoop = new AnimationTimer() {
+            @Override
+            public void handle(long l) {
+                update();
+            }
+        };
+        load();
+        gameLoop.start();
+    }
+
+    @FXML
+    void pressed(MouseEvent event) {
+        birdComponent.fly();
+        accelerationTime = 0;
+
+    }
+
+
+    //Called every game frame
+    private void update() {
+        gameTime++;
+        accelerationTime++;
+        double yDelta = 0.03;
+        birdComponent.moveBirdY(yDelta * accelerationTime);
+
+        if(pointChecker(obstacles, bird)){
+            scoreCounter++;
+            score.setText(String.valueOf(scoreCounter));
+        }
+
+        obstaclesHandler.moveObstacles(obstacles);
+        if(gameTime % 400 == 0){
+            obstacles.addAll(obstaclesHandler.createObstacles());
+        }
+
+        if(birdComponent.isBirdDead(obstacles, plane)){
+            resetGame();
+        }
+    }
+
+    //Everything called once, at the game start
+    private void load(){
+        obstacles.addAll(obstaclesHandler.createObstacles());
+    }
+
+    private void resetGame(){
+        bird.setY(0);
+        plane.getChildren().removeAll(obstacles);
+        obstacles.clear();
+        gameTime = 0;
+        accelerationTime = 0;
+        scoreCounter = 0;
+        score.setText(String.valueOf(scoreCounter));
+        FXMLLoader fxmlLoader = new FXMLLoader(Controller.class.getResource("restartview.fxml"));
+        Parent parent = null;
+        try {
+            parent = fxmlLoader.load();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Scene scene = new Scene(parent);
+        Stage app_stage = (Stage) bird.getScene().getWindow();
+        app_stage.setScene(scene);
+        app_stage.show();
+    }
+
+
+
+    private boolean pointChecker(ArrayList<Rectangle> obstacles, ImageView bird){
+        for (Rectangle obstacle: obstacles) {
+            int birdPositionX = (int) (bird.getLayoutX() + bird.getX());
+            if(((int)(obstacle.getLayoutX() + obstacle.getX()) == birdPositionX)){
+                return true;
+            }
+        }
+        return false;
+    }
+}
